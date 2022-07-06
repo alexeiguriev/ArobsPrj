31d94cc8803a3702da8455494bb8a22b4f9f580d
alexeiguriev <alexeiguriev1@gmail.com>
2022-07-06 16:21:27 +0300

    Timer test

--- ArobsPrj/main.c
+++ ArobsPrj/main.c
@@ -6,33 +6,46 @@
 //#include <xc.h>
 #include <stdio.h>
 #include "LCD1602_lib.h"
+#include "TimerSw.h"
 
 void init_PWM_Timer();
 int PWMstate = 0;
 int PWMstateIntermadiar = 0;
+TimerSwInitParam timerSwInitParam;
+TimerSwHandle timerSwHandle;
 
 #define LED PD4
 
 ISR (TIMER1_OVF_vect)    // Timer1 ISR
 {
-	PORTD ^= (1 << LED);
-	PWMstateIntermadiar++;
-	if(PWMstateIntermadiar >= 1000)
-	{
-		PWMstate++;
-		PWMstateIntermadiar = 0;
-	}
+	timerSwInitParam.interval++;
 	TCNT1 = 49535;   // for 1 sec at 16 MHz  63974
 }
 
 int main(void)
 {
+	StatusError err;
+	timerSwInitParam.interval = 0;
+	
+	err = TimerSwInit(&timerSwInitParam,&timerSwHandle);
+	
+	if (err == StatusErrNone)
+	{		
+		TimerSwStartup(&timerSwHandle,1000);
+	}
 	lcd_init(); // Initializare LCD
 	lcd_clear();
 	init_PWM_Timer();
 	while(1)
 	{
 
+		err = TimerSwIsExpired(&timerSwHandle);
+		if (err == StatusErrTime)
+		{
+			PORTD ^= (1 << LED);
+			PWMstate++;
+			TimerSwStartup(&timerSwHandle,1000);
+		}
 		set_pos(0,0);
 		lcd_Print_str("Hello World!");
 		set_pos(0,1);
