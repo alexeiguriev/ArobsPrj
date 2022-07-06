6bb70706bdd20cafad66830aaa6229c0db14b223
alexeiguriev <59488277+alexeiguriev@users.noreply.github.com>
2022-07-06 12:52:35 +0300

    First commit
    First commit

---
+++ ArobsPrj/main.c
@@ -0,0 +1,18 @@
+/*
+ * ArobsPrj.c
+ *
+ * Created: 7/6/2022 12:49:31 PM
+ * Author : alexei.guriev
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
